ac78c22eebdaba3531f453865fae213d1510697c
saif <Saifshoubaki10@gmail.com>
2023-11-02 06:50:09 +0000

    idk

--- RWG/Models/RegisterViewModel.cs
+++ RWG/Models/RegisterViewModel.cs
@@ -3,6 +3,7 @@ namespace RWG.Models
 {
 	public class RegisterViewModel
 	{
+        //sets the getters and setters for each of these properties to use in other classes
 		public string Email { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
