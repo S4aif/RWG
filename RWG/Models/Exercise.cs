cf17e3b26140e59b91e3cdc6de772a57191f4112
saif <Saifshoubaki10@gmail.com>
2023-11-04 10:28:48 +0000

    Exercise name

--- RWG/Models/Exercise.cs
+++ RWG/Models/Exercise.cs
@@ -4,6 +4,7 @@ namespace RWG.Models
 	public class Exercise
 	{
 		public int Id { get; set; }
+		public string Name { get; set; }
 		public string Video { get; set; }
 		public string Description { get; set; }
 		//One to many relationship, Exercise to WorkoutExercise
